b8c6d7f5074c57f92d2c23942d48bea4b06cc0cf
Seikun Kambashi <seikun_lee@hotmail.com>
2016-01-31 21:54:01 -0500

    player skeleton

---
+++ Player.lua
@@ -0,0 +1,17 @@
+Class = require "lib/hump.class"
+
+local Player = Class {
+    init = function(self, world, x, y)
+        self.entity = world:addCollisionClass('player')
+        self.x = x
+        self.y = y
+    end
+}
+
+function Player:update(dt)
+end
+
+function Player:draw()
+end
+
+return Player
