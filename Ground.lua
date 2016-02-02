cf6f3bf2328d56fbbf27522252af590befab0e6f
Hamdan Javeed <hamdan@hamdanjaveed.com>
2016-02-01 21:06:13 -0500

    Dont make walls ground

--- Ground.lua
+++ Ground.lua
@@ -2,12 +2,12 @@ local Class = require "lib/middleclass.middleclass"
 
 local Ground = Class("Ground")
 
-function Ground:initialize(world, x, y, w, h)
+function Ground:initialize(world, x, y, w, h, isWall)
     self.width = w
     self.height = h
 
     self.collider = world:newRectangleCollider(x, y, w, h, {
-        collision_class = "Ground",
+        collision_class = isWall and "Wall" or "Ground",
         body_type = "static"
     })
 end
